a0d4c5c4379e2a53b42facd88da8fcfbb4cca3dc
baimamboukar <baimamboukar@gmail.com>
2022-07-24 22:43:45 +0100

    palette widget

--- lib/src/utils/palette.dart
+++ lib/src/utils/palette.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 class Palette {
-  static const Color primary = Color(0xFF337669);
-  static const Color secondary = Color(0xFFA1A1A1);
+  static const Color primary = Color(0xFF003B5C);
+  static const Color secondary = Color(0xFF007681);
+  static const Color tertiary = Color(0xFF7D2248);
   static const Color light = Color(0xFFEAEAEA);
   static const Color dark = Color(0xFF797D7A);
-  static const Color danger = Color.fromARGB(255, 250, 12, 12);
 }
