0acbfe3aef72594ff5df12362157b84f9ff7cdfb
baimamboukar <baimamboukar@gmail.com>
2022-07-24 22:43:54 +0100

    test widgets

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:hymnes_adventistes/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const HymnesAdventistes());
+    await tester.pumpWidget(HymnesAdventistes());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
