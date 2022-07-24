d0084e02a58ce89c9ed50b2ad5f95107cbaed039
baimamboukar <baimamboukar@gmail.com>
2022-07-24 20:55:04 +0100

    home screen

---
+++ lib/src/screens/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class Home extends ConsumerWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Hynes Adventistes"),
+      ),
+    );
+  }
+}
