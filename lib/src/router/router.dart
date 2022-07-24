05227d0d9d296ccbfb15b81357711aa4fb71ebe4
baimamboukar <baimamboukar@gmail.com>
2022-07-24 22:43:08 +0100

    router

--- lib/src/router/router.dart
+++ lib/src/router/router.dart
@@ -2,6 +2,6 @@ import 'package:auto_route/auto_route.dart';
 import 'package:hymnes_adventistes/src/screens/index.dart';
 
 @AdaptiveAutoRouter(replaceInRouteName: 'Page,Route', routes: <AutoRoute>[
-  AutoRoute(page: Home, path: '/home', initial: false),
+  AutoRoute(page: Home, path: '/home', initial: true),
 ])
 class $AppRouter {}
