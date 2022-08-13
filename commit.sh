c93555eaff58dfd43f6bf7acbb455889d86acb3f
baimamboukar <baimamboukar@gmail.com>
2022-08-13 16:26:29 +0100

    adding commit.sh ✅

--- commit.sh
+++ commit.sh
@@ -1,10 +1,3 @@
- for file in $(git ls-files --others --exclude-standard)
- do
-    git add $file
-    git commit -m "adding $file ✅"
-    git push -u origin main
- done
-
 
 for file in $(git ls-files --modified --exclude-standard)
  do
