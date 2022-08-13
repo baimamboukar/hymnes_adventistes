902e6802ccb4b1905125fd1939a7895ced7e42ff
baimamboukar <baimamboukar@gmail.com>
2022-08-13 23:00:01 +0100

    adding lib/src/utils/palette.dart ✅

--- lib/src/utils/palette.dart
+++ lib/src/utils/palette.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 class Palette {
-  static Color primary = Color(Hive.box('settings').get('color'));
+  static Color primary = Color(Hive.box('settings').get('color') ?? 0xFF007681);
   static const Color secondary = Color(0xFF007681);
   static const Color tertiary = Color(0xFF7D2248);
   static const Color light = Color(0xFFEAEAEA);
