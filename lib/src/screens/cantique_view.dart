b54a94927fc8182093403644925d941602b727c3
baimamboukar <baimamboukar@gmail.com>
2022-09-09 16:16:58 +0100

    adding lib/src/screens/cantique_view.dart ✅

--- lib/src/screens/cantique_view.dart
+++ lib/src/screens/cantique_view.dart
@@ -8,6 +8,8 @@ import 'package:hymnes_adventistes/src/models/cantique.dart';
 import 'package:hymnes_adventistes/src/utils/decorations.dart';
 import 'package:hymnes_adventistes/src/utils/index.dart';
 import 'package:hymnes_adventistes/src/utils/text_styles.dart';
+import 'package:line_icons/line_icons.dart';
+import 'package:lottie/lottie.dart';
 
 final activeLanguageProvider = StateProvider<int>((ref) {
   return 0;
@@ -23,123 +25,187 @@ class CantiqueView extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final activeLanguage = ref.watch(activeLanguageProvider.state);
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Palette.primary,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(
-            bottom: Radius.circular(18),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Palette.primary,
+          // shape: const RoundedRectangleBorder(
+          //   borderRadius: BorderRadius.vertical(
+          //     bottom: Radius.circular(18),
+          //   ),
+          // ),
+          leading: IconButton(
+            icon: const Icon(
+              Icons.arrow_back_ios,
+              color: Palette.light,
+            ),
+            onPressed: () => Navigator.pop(context),
           ),
-        ),
-        title: Text(
-          cantique.title,
-          style: TextStyles.designText(
-              bold: false, color: Palette.light, size: 14),
-        ),
-        bottom: PreferredSize(
-          preferredSize: Size(context.screenWidth, 50),
-          child: Padding(
-            padding: const EdgeInsets.only(bottom: 8.0),
-            child: Container(
-              width: context.screenWidth * .9,
-              height: 50,
-              decoration:
-                  Decorations.decorateBox(radius: 32, color: Palette.light)
-                      .copyWith(
-                border: Border.all(color: Palette.light, width: 0),
+          title: Text(
+            cantique.title,
+            style: TextStyles.designText(
+                bold: false, color: Palette.light, size: 14),
+          ),
+          bottom: TabBar(
+            indicatorColor: Palette.light,
+            indicatorWeight: 2,
+            indicatorPadding:
+                const EdgeInsets.symmetric(horizontal: 4, vertical: 1),
+            tabs: [
+              Tab(
+                child: Text(
+                  "Français",
+                  style: TextStyles.designText(
+                      bold: false, color: Palette.light, size: 12),
+                ),
               ),
+              Tab(
+                child: Text(
+                  "Fulfulde",
+                  style: TextStyles.designText(
+                      bold: false, color: Palette.light, size: 12),
+                ),
+              ),
+              Tab(
+                child: Text(
+                  "English",
+                  style: TextStyles.designText(
+                      bold: false, color: Palette.light, size: 12),
+                ),
+              ),
+            ],
+          ),
+          // bottom: PreferredSize(
+          //   preferredSize: Size(context.screenWidth, 50),
+          //   child: Padding(
+          //     padding: const EdgeInsets.only(bottom: 8.0),
+          //     child: Container(
+          //       width: context.screenWidth * .9,
+          //       height: 50,
+          //       decoration:
+          //           Decorations.decorateBox(radius: 32, color: Palette.light)
+          //               .copyWith(
+          //         border: Border.all(color: Palette.light, width: 0),
+          //       ),
+          //       child: Row(
+          //         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          //         children: [
+          //           LabelChip(
+          //             activeLanguage: activeLanguage,
+          //             language: "Fulfulde",
+          //             position: 0,
+          //           ),
+          //           LabelChip(
+          //             activeLanguage: activeLanguage,
+          //             language: "Francais",
+          //             position: 1,
+          //           ),
+          //           LabelChip(
+          //             activeLanguage: activeLanguage,
+          //             language: "English",
+          //             position: 2,
+          //           ),
+          //         ],
+          //       ),
+          //     ),
+          //   ),
+          // ),
+        ),
+        body: TabBarView(
+          children: [
+            buildCantiqueBody(cantique: cantique),
+            buildCantiqueBody(),
+            buildCantiqueBody(),
+          ],
+        ),
+        bottomSheet: SizedBox(
+          height: 70,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8) +
+                const EdgeInsets.only(bottom: 8, top: 4),
+            child: GestureDetector(
+              onTap: () {},
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  LabelChip(
-                    activeLanguage: activeLanguage,
-                    language: "Fulfulde",
-                    position: 0,
-                  ),
-                  LabelChip(
-                    activeLanguage: activeLanguage,
-                    language: "Francais",
-                    position: 1,
-                  ),
-                  LabelChip(
-                    activeLanguage: activeLanguage,
-                    language: "English",
-                    position: 2,
-                  ),
+                  Lottie.asset("assets/animations/play.json",
+                      height: 55, width: 55),
+                  Lottie.asset("assets/animations/waves.json", height: 55),
                 ],
               ),
             ),
           ),
         ),
       ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 20,
+    );
+  }
+
+  Widget buildCantiqueBody({CantiqueModel? cantique}) {
+    if (cantique == null) {
+      return const Center(
+        child: Text("Pas de cantique"),
+      );
+    }
+    return SingleChildScrollView(
+      child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              cantique.title,
+              style: TextStyles.designText(bold: false, size: 18),
+            ),
+            Text(
+              "${cantique.number} - Hymnes & Louanges",
+              style: TextStyles.designText(
+                  bold: false, color: Palette.dark, size: 12),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            for (var strophe in cantique.strophes)
+              Column(
+                children: [
+                  Text(strophe.number.toString()),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  ...strophe.vers.map(
+                    (vers) {
+                      return Text(
+                        vers.content,
+                        textAlign: TextAlign.justify,
+                        style: TextStyles.designText(
+                                bold: false,
+                                size: 15,
+                                color: cantique.refrain &&
+                                        strophe.number == "refrain"
+                                    ? Palette.primary
+                                    : Palette.dark)
+                            .copyWith(
+                                fontStyle: cantique.refrain &&
+                                        strophe.number == "refrain"
+                                    ? FontStyle.italic
+                                    : FontStyle.normal),
+                      );
+                    },
+                  ),
+                  const SizedBox(
+                    height: 14,
+                  ),
+                ],
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              Text(
-                cantique.title,
-                style: TextStyles.designText(bold: false, size: 18),
-              ),
-              Text(
-                "${cantique.number} - Hymnes & Louanges",
-                style: TextStyles.designText(
-                    bold: false, color: Palette.dark, size: 12),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              for (var strophe in cantique.strophes)
-                Column(
-                  children: [
-                    Text(strophe.number.toString()),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    ...strophe.vers.map(
-                      (vers) {
-                        return Text(
-                          vers.content,
-                          textAlign: TextAlign.justify,
-                          style: TextStyles.designText(
-                                  bold: false,
-                                  size: 15,
-                                  color: cantique.refrain &&
-                                          strophe.number == "refrain"
-                                      ? Palette.primary
-                                      : Palette.dark)
-                              .copyWith(
-                                  fontStyle: cantique.refrain &&
-                                          strophe.number == "refrain"
-                                      ? FontStyle.italic
-                                      : FontStyle.normal),
-                        );
-                      },
-                    ),
-                    const SizedBox(
-                      height: 14,
-                    ),
-                  ],
-                ),
-              IconButton(
-                icon: const Icon(Icons.my_library_music, size: 68),
-                onPressed: () {
-                  AudioPlayer player = AudioPlayer();
-                  AssetSource src = AssetSource("sounds/101.midi");
-                  player.play(src);
-                },
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-            ],
-          ),
+            const SizedBox(
+              height: 60,
+            ),
+          ],
         ),
       ),
     );
