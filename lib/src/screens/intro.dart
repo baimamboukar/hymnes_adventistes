ef2998aeb85f9d28cf78b2274b50a7d3623cfd69
baimamboukar <baimamboukar@gmail.com>
2022-09-09 16:16:58 +0100

    adding lib/src/screens/intro.dart ✅

--- lib/src/screens/intro.dart
+++ lib/src/screens/intro.dart
@@ -1,3 +1,5 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart'
     show WidgetRef, ConsumerWidget;
@@ -11,47 +13,110 @@ class Intro extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Text("Hymnes Adventistes", style: TextStyles.title),
-        const SizedBox(height: 18),
-        Container(
-            height: 50.0,
-            width: (context.screenWidth - 50) * .8,
-            decoration: BoxDecoration(
-              color: Palette.light,
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            child: Center(
-              child: TextFormField(
-                decoration: InputDecoration(
-                  hintText: context.loc.adventist,
-                  border: InputBorder.none,
-                  prefixIcon: const Icon(
-                    Icons.library_music,
-                    color: Palette.tertiary,
+    // ignore: no_leading_underscores_for_local_identifiers
+    final _numberController = TextEditingController();
+    return SingleChildScrollView(
+      controller: ScrollController(),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text("Hymnes Adventistes", style: TextStyles.title),
+          const SizedBox(height: 18),
+          Row(
+            children: [
+              Container(
+                height: 60.0,
+                width: (context.screenWidth - 50) * .6,
+                decoration: BoxDecoration(
+                  color: Palette.light,
+                  borderRadius: BorderRadius.circular(14.0),
+                ),
+                child: Center(
+                  child: TextFormField(
+                    controller: _numberController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      hintText: "Numero cantique",
+                      hintStyle: TextStyles.designText(
+                          bold: false, size: 12, color: Palette.dark),
+                      border: InputBorder.none,
+                      prefixIcon: const Icon(
+                        Icons.library_music,
+                        color: Palette.dark,
+                      ),
+                    ),
                   ),
                 ),
               ),
-            )),
-        const SizedBox(
-          height: 20,
-        ),
-        const Box(),
-        const CardTile(
-          title: "Fulfulde",
-          subtitle: "Defterre Gimmi be fulfulde",
-        ),
-        const CardTile(
-          title: "Francais",
-          subtitle: "Hymnes & Louanges",
-        ),
-        const CardTile(
-          title: "English",
-          subtitle: "SDA Hymnals",
-        ),
-      ],
+              const SizedBox(width: 10),
+              FloatingActionButton(
+                onPressed: () {
+                  if (int.parse(_numberController.value.text) > 695) {
+                    // invalid cantique
+                  }
+                  if (int.parse(_numberController.value.text) > 654) {
+                    // go to cantique view with english
+                  }
+
+                  if (_numberController.text.isNotEmpty &&
+                      int.parse(_numberController.value.text) > 0 &&
+                      int.parse(_numberController.value.text) <= 695) {
+                    // choose language
+                    if (int.parse(_numberController.value.text) < 655) {
+                      showCupertinoModalPopup(
+                        context: context,
+                        builder: (context) => CupertinoActionSheet(
+                          title: const Text("Choisir la langue"),
+                          actions: [
+                            if (int.parse(_numberController.value.text) <= 107)
+                              CupertinoActionSheetAction(
+                                isDefaultAction: true,
+                                onPressed: () {},
+                                child: const Text("Fulfulde"),
+                              ),
+                            CupertinoActionSheetAction(
+                              onPressed: () {},
+                              child: const Text("Francais"),
+                            ),
+                            CupertinoActionSheetAction(
+                              onPressed: () {},
+                              child: const Text("English"),
+                            ),
+                          ],
+                          cancelButton: CupertinoActionSheetAction(
+                            isDestructiveAction: true,
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: const Text("Annuler"),
+                          ),
+                        ),
+                      );
+                    }
+                  }
+                },
+                child: const Center(child: Text("Go")),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Box(),
+          const CardTile(
+            title: "Fulfulde",
+            subtitle: "Defterre Gimmi be fulfulde",
+          ),
+          const CardTile(
+            title: "Francais",
+            subtitle: "Hymnes & Louanges",
+          ),
+          const CardTile(
+            title: "English",
+            subtitle: "SDA Hymnals",
+          ),
+        ],
+      ),
     );
   }
 }
