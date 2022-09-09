9e33868f01a5abb67e0913077efc9120460f6e42
baimamboukar <baimamboukar@gmail.com>
2022-09-09 09:22:21 +0100

    adding processing/clean-en.py ✅

---
+++ processing/clean-en.py
@@ -0,0 +1,31 @@
+import json
+import pprint as printer
+
+file = open("../assets/data/hymns-en.json")
+data = json.loads(file.read())
+
+for index in range(64, 65):
+    model = {}
+    cantique = data["songs"][str(index)]
+    model["number"] = int(cantique["id"])
+    model["parts"] = list(cantique["stanzas"].keys())
+    model["slides"] = list(cantique["stanzas"].keys())
+    model["title"] = cantique["title"]
+    for x in model["parts"]:
+        model[x] = cantique["stanzas"][x]
+    if model["1"].__contains__("Refrain"):
+        tmp = []
+        for x in model["parts"]:
+            tmp.append(x)
+            tmp.append("refrain")
+        model["slides"] = tmp
+        model["parts"].insert(1, "refrain")
+        model["refrain"] = model["1"].split("Refrain")[1]
+        model["1"] = model["1"].split("Refrain")[0]
+    model["references"] = {"en": int(cantique["number"]), "fr": 0, "ful": 0}
+    if (int(model["number"])) in ful2fr:
+        full = ful2fr[int(cantique["number"])]
+        cantique["references"] = {"en": cantique["number"], "fr": 0, "ful": full}
+    for key in model["parts"]:
+        model[key] = [clean for clean in model[key].split(";")]
+    printer.pprint(model)
