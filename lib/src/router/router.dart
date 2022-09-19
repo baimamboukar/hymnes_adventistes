63dc3c97718f5a3f3a27cfc186a5ec4177702935
baimamboukar <baimamboukar@gmail.com>
2022-09-19 18:15:03 +0100

    adding lib/src/router/router.dart ✅

--- lib/src/router/router.dart
+++ lib/src/router/router.dart
@@ -1,9 +1,11 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:hymnes_adventistes/src/screens/cantique_view.dart';
+import 'package:hymnes_adventistes/src/screens/hymn_overview.dart';
 import 'package:hymnes_adventistes/src/screens/index.dart';
 
 @AdaptiveAutoRouter(replaceInRouteName: 'Page,Route', routes: <AutoRoute>[
   AutoRoute(page: Home, path: '/home', initial: true),
   AutoRoute(page: CantiqueView, path: '/cantique', initial: false),
+  AutoRoute(page: HymnOverview, path: '/hymn-overview', initial: false),
 ])
 class $AppRouter {}
