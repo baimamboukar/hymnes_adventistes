f9a9156f90077e529898d02d2fa995b4de05e42b
baimamboukar <baimamboukar@gmail.com>
2022-12-01 11:25:43 +0100

    correctig pr color state

--- commit.sh
+++ commit.sh
@@ -2,7 +2,7 @@
 for file in $(git ls-files --modified --others --exclude-standard)
  do
     git add $file
-    git commit -m "adding $file ✅"
+    git commit -m "$1"
  done
 
 git push -u origin main
