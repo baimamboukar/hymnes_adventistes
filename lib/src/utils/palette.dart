5d650c63e62b05faceda59b5de138f75b018b81c
baimamboukar <baimamboukar@gmail.com>
2022-12-01 11:25:43 +0100

    correctig pr color state

--- lib/src/utils/palette.dart
+++ lib/src/utils/palette.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 class Palette {
-  static Color primary = Color(Hive.box('settings').get('color') ?? 0xFF007681);
+  static ValueNotifier<Color> primary =
+      ValueNotifier<Color>((Hive.box('settings').get('color') ?? 0xFF007681));
   static const Color secondary = Color(0xFF007681);
   static const Color tertiary = Color(0xFF7D2248);
   static const Color light = Color(0xFFEAEAEA);
   static const Color dark = Color(0xFF797D7A);
+  static ValueNotifier<Color> backgroundDark =
+      ValueNotifier(const Color(0xFF16213E));
 }
