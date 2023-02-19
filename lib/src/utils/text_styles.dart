c0e899f444b4f580a4d3d282d151725765662902
baimamboukar <baimamboukar@gmail.com>
2023-02-19 17:34:58 +0100

    moved to material 3

--- lib/src/utils/text_styles.dart
+++ lib/src/utils/text_styles.dart
@@ -3,15 +3,17 @@ import 'package:hymnes_adventistes/src/utils/index.dart';
 
 class TextStyles {
   static TextStyle get title => TextStyle(
-      fontSize: 22.0,
-      fontWeight: FontWeight.bold,
-      color: Palette.primary.value);
+        fontSize: 22.0,
+        fontWeight: FontWeight.bold,
+        color: Palette.primary.value,
+      );
   static TextStyle get body => const TextStyle(
       fontSize: 16.0, fontWeight: FontWeight.normal, color: Palette.light);
   static TextStyle designText(
           {required bool bold, required double size, Color? color}) =>
       TextStyle(
-          fontSize: size,
-          fontWeight: bold ? FontWeight.bold : FontWeight.normal,
-          color: color ?? Palette.primary.value);
+        fontSize: size,
+        fontWeight: bold ? FontWeight.bold : FontWeight.normal,
+        color: color ?? Palette.primary.value,
+      );
 }
