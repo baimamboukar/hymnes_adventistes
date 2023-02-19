a0d573c49094c241848f657a2a7c513a5de7e009
baimamboukar <baimamboukar@gmail.com>
2023-02-19 17:34:58 +0100

    moved to material 3

--- lib/src/widgets/card_tile.dart
+++ lib/src/widgets/card_tile.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:hymnes_adventistes/src/extensions/theme.dart';
 
 import 'package:hymnes_adventistes/src/utils/index.dart';
 import 'package:hymnes_adventistes/src/utils/text_styles.dart';
@@ -23,7 +24,7 @@ class CardTile extends StatelessWidget {
         title: Text(
           title,
           style: TextStyles.designText(
-              bold: false, color: Palette.primary.value, size: 14),
+              bold: false, color: context.colorScheme.primary, size: 14),
         ),
         subtitle: Text(subtitle),
       ),
