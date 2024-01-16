552add88c7879e1b055872d765fe06adff6a8536
baimamboukar <baimamboukar@gmail.com>
2024-01-16 23:51:17 +0100

    deps: add additional deps

--- Makefile
+++ Makefile
@@ -1,10 +1,5 @@
+install:
+	flutter pub add dio flutter_bloc get_it flutter_dotenv glass_kit intl introduction_screen heroicons freezed freezed_annotation path_provider permission_handler redacted hydrated_bloc uuid flutter_dotenv flutter_animate flex_seed_scheme
 
-push:
-   for file in $(git ls-files --modified --others --exclude-standard)
-   do
-      git add $file
-      git commit -m "$1"
-   done
-
-  git push -u origin main
-
+installdev:
+	flutter pub add --dev very_good_analysis flutter_launcher_icons flutter_native_splash
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -8,10 +8,13 @@
 
 #include <audioplayers_windows/audioplayers_windows_plugin.h>
 #include <file_selector_windows/file_selector_windows.h>
+#include <permission_handler_windows/permission_handler_windows_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   AudioplayersWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("AudioplayersWindowsPlugin"));
   FileSelectorWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FileSelectorWindows"));
+  PermissionHandlerWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
 }
