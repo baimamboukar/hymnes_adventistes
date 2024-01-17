f126cb41fef79ebb51e03cfaeea5f2758a5ec314
baimamboukar <baimamboukar@gmail.com>
2024-01-17 15:08:27 +0100

    chore: add `apprunner`

---
+++ lib/src/app/app_runner.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class AppRunner {
+  /// A static method to be executed before the app runs.
+  static Future<void> preRunAction() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await dotenv.load();
+  }
+
+  /// A static method to be executed while the app runs.
+  static Future<void> runAction() async {}
+
+  /// A static method to be executed after the app runs.
+  static Future<void> postRunAction() async {}
+}
