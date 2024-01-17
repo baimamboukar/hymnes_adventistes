571eb7e1719a53710c8bfd5b628d67ad0696e2ae
baimamboukar <baimamboukar@gmail.com>
2024-01-17 15:09:51 +0100

    test: fix materialapp name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:hymnes_adventistes/main.dart';
+import 'package:hymnes_adventistes/src/app/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const SabbathSongs());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
