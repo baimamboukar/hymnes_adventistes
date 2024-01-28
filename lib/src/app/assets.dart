c6fc08cb9f794258dd3d30ba8201f9b9e4f40a22
baimamboukar <baimamboukar@gmail.com>
2024-01-28 22:52:13 +0100

    chore: build asssets gen

---
+++ lib/src/app/assets.dart
@@ -0,0 +1,584 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsAnimationsNotes
+  /// assets/animations/notes.json
+  static const String assetsAnimationsNotes = "assets/animations/notes.json";
+
+  /// Assets for assetsAnimationsPlay
+  /// assets/animations/play.json
+  static const String assetsAnimationsPlay = "assets/animations/play.json";
+
+  /// Assets for assetsAnimationsWaves
+  /// assets/animations/waves.json
+  static const String assetsAnimationsWaves = "assets/animations/waves.json";
+
+  /// Assets for assetsDataHymns
+  /// assets/data/hymns.json
+  static const String assetsDataHymns = "assets/data/hymns.json";
+
+  /// Assets for assetsDataHymnsEn
+  /// assets/data/hymns-en.json
+  static const String assetsDataHymnsEn = "assets/data/hymns-en.json";
+
+  /// Assets for assetsDataHymnsEn
+  /// assets/data/hymns_en.json
+  static const String assetsDataHymnsEn = "assets/data/hymns_en.json";
+
+  /// Assets for assetsDataHymnsFr
+  /// assets/data/hymns_fr.json
+  static const String assetsDataHymnsFr = "assets/data/hymns_fr.json";
+
+  /// Assets for assetsDataHymnsFul
+  /// assets/data/hymns-ful.json
+  static const String assetsDataHymnsFul = "assets/data/hymns-ful.json";
+
+  /// Assets for assetsDataHymnsFulClean
+  /// assets/data/hymns-ful-clean.json
+  static const String assetsDataHymnsFulClean = "assets/data/hymns-ful-clean.json";
+
+  /// Assets for assetsDataHymnsFull
+  /// assets/data/hymns-full.json
+  static const String assetsDataHymnsFull = "assets/data/hymns-full.json";
+
+  /// Assets for assetsDataHymnsFull
+  /// assets/data/hymns_full.json
+  static const String assetsDataHymnsFull = "assets/data/hymns_full.json";
+
+  /// Assets for assetsDataTitles
+  /// assets/data/titles.json
+  static const String assetsDataTitles = "assets/data/titles.json";
+
+  /// Assets for assetsImagesCover
+  /// assets/images/cover.png
+  static const String assetsImagesCover = "assets/images/cover.png";
+
+  /// Assets for assetsImagesLogo
+  /// assets/images/logo.png
+  static const String assetsImagesLogo = "assets/images/logo.png";
+
+  /// Assets for assetsImagesLogox
+  /// assets/images/logox.png
+  static const String assetsImagesLogox = "assets/images/logox.png";
+
+  /// Assets for assetsSounds002
+  /// assets/sounds/002.midi
+  static const String assetsSounds002 = "assets/sounds/002.midi";
+
+  /// Assets for assetsSounds004
+  /// assets/sounds/004.midi
+  static const String assetsSounds004 = "assets/sounds/004.midi";
+
+  /// Assets for assetsSounds024
+  /// assets/sounds/024.midi
+  static const String assetsSounds024 = "assets/sounds/024.midi";
+
+  /// Assets for assetsSounds025
+  /// assets/sounds/025.midi
+  static const String assetsSounds025 = "assets/sounds/025.midi";
+
+  /// Assets for assetsSounds029
+  /// assets/sounds/029.midi
+  static const String assetsSounds029 = "assets/sounds/029.midi";
+
+  /// Assets for assetsSounds036
+  /// assets/sounds/036.midi
+  static const String assetsSounds036 = "assets/sounds/036.midi";
+
+  /// Assets for assetsSounds037
+  /// assets/sounds/037.midi
+  static const String assetsSounds037 = "assets/sounds/037.midi";
+
+  /// Assets for assetsSounds038
+  /// assets/sounds/038.midi
+  static const String assetsSounds038 = "assets/sounds/038.midi";
+
+  /// Assets for assetsSounds039
+  /// assets/sounds/039.midi
+  static const String assetsSounds039 = "assets/sounds/039.midi";
+
+  /// Assets for assetsSounds040
+  /// assets/sounds/040.midi
+  static const String assetsSounds040 = "assets/sounds/040.midi";
+
+  /// Assets for assetsSounds041
+  /// assets/sounds/041.midi
+  static const String assetsSounds041 = "assets/sounds/041.midi";
+
+  /// Assets for assetsSounds042
+  /// assets/sounds/042.midi
+  static const String assetsSounds042 = "assets/sounds/042.midi";
+
+  /// Assets for assetsSounds044
+  /// assets/sounds/044.midi
+  static const String assetsSounds044 = "assets/sounds/044.midi";
+
+  /// Assets for assetsSounds046
+  /// assets/sounds/046.midi
+  static const String assetsSounds046 = "assets/sounds/046.midi";
+
+  /// Assets for assetsSounds047
+  /// assets/sounds/047.midi
+  static const String assetsSounds047 = "assets/sounds/047.midi";
+
+  /// Assets for assetsSounds049
+  /// assets/sounds/049.midi
+  static const String assetsSounds049 = "assets/sounds/049.midi";
+
+  /// Assets for assetsSounds050
+  /// assets/sounds/050.midi
+  static const String assetsSounds050 = "assets/sounds/050.midi";
+
+  /// Assets for assetsSounds054
+  /// assets/sounds/054.midi
+  static const String assetsSounds054 = "assets/sounds/054.midi";
+
+  /// Assets for assetsSounds055
+  /// assets/sounds/055.midi
+  static const String assetsSounds055 = "assets/sounds/055.midi";
+
+  /// Assets for assetsSounds056
+  /// assets/sounds/056.midi
+  static const String assetsSounds056 = "assets/sounds/056.midi";
+
+  /// Assets for assetsSounds058
+  /// assets/sounds/058.midi
+  static const String assetsSounds058 = "assets/sounds/058.midi";
+
+  /// Assets for assetsSounds059
+  /// assets/sounds/059.midi
+  static const String assetsSounds059 = "assets/sounds/059.midi";
+
+  /// Assets for assetsSounds060
+  /// assets/sounds/060.midi
+  static const String assetsSounds060 = "assets/sounds/060.midi";
+
+  /// Assets for assetsSounds061
+  /// assets/sounds/061.midi
+  static const String assetsSounds061 = "assets/sounds/061.midi";
+
+  /// Assets for assetsSounds062
+  /// assets/sounds/062.midi
+  static const String assetsSounds062 = "assets/sounds/062.midi";
+
+  /// Assets for assetsSounds063
+  /// assets/sounds/063.midi
+  static const String assetsSounds063 = "assets/sounds/063.midi";
+
+  /// Assets for assetsSounds064
+  /// assets/sounds/064.midi
+  static const String assetsSounds064 = "assets/sounds/064.midi";
+
+  /// Assets for assetsSounds065
+  /// assets/sounds/065.midi
+  static const String assetsSounds065 = "assets/sounds/065.midi";
+
+  /// Assets for assetsSounds066
+  /// assets/sounds/066.midi
+  static const String assetsSounds066 = "assets/sounds/066.midi";
+
+  /// Assets for assetsSounds067
+  /// assets/sounds/067.midi
+  static const String assetsSounds067 = "assets/sounds/067.midi";
+
+  /// Assets for assetsSounds070
+  /// assets/sounds/070.midi
+  static const String assetsSounds070 = "assets/sounds/070.midi";
+
+  /// Assets for assetsSounds072
+  /// assets/sounds/072.midi
+  static const String assetsSounds072 = "assets/sounds/072.midi";
+
+  /// Assets for assetsSounds073
+  /// assets/sounds/073.midi
+  static const String assetsSounds073 = "assets/sounds/073.midi";
+
+  /// Assets for assetsSounds074
+  /// assets/sounds/074.midi
+  static const String assetsSounds074 = "assets/sounds/074.midi";
+
+  /// Assets for assetsSounds075
+  /// assets/sounds/075.midi
+  static const String assetsSounds075 = "assets/sounds/075.midi";
+
+  /// Assets for assetsSounds076
+  /// assets/sounds/076.midi
+  static const String assetsSounds076 = "assets/sounds/076.midi";
+
+  /// Assets for assetsSounds077
+  /// assets/sounds/077.midi
+  static const String assetsSounds077 = "assets/sounds/077.midi";
+
+  /// Assets for assetsSounds080
+  /// assets/sounds/080.midi
+  static const String assetsSounds080 = "assets/sounds/080.midi";
+
+  /// Assets for assetsSounds083
+  /// assets/sounds/083.midi
+  static const String assetsSounds083 = "assets/sounds/083.midi";
+
+  /// Assets for assetsSounds084
+  /// assets/sounds/084.midi
+  static const String assetsSounds084 = "assets/sounds/084.midi";
+
+  /// Assets for assetsSounds085
+  /// assets/sounds/085.midi
+  static const String assetsSounds085 = "assets/sounds/085.midi";
+
+  /// Assets for assetsSounds087
+  /// assets/sounds/087.midi
+  static const String assetsSounds087 = "assets/sounds/087.midi";
+
+  /// Assets for assetsSounds088
+  /// assets/sounds/088.midi
+  static const String assetsSounds088 = "assets/sounds/088.midi";
+
+  /// Assets for assetsSounds089
+  /// assets/sounds/089.midi
+  static const String assetsSounds089 = "assets/sounds/089.midi";
+
+  /// Assets for assetsSounds094
+  /// assets/sounds/094.midi
+  static const String assetsSounds094 = "assets/sounds/094.midi";
+
+  /// Assets for assetsSounds097
+  /// assets/sounds/097.midi
+  static const String assetsSounds097 = "assets/sounds/097.midi";
+
+  /// Assets for assetsSounds100
+  /// assets/sounds/100.midi
+  static const String assetsSounds100 = "assets/sounds/100.midi";
+
+  /// Assets for assetsSounds101
+  /// assets/sounds/101.midi
+  static const String assetsSounds101 = "assets/sounds/101.midi";
+
+  /// Assets for assetsSounds102
+  /// assets/sounds/102.midi
+  static const String assetsSounds102 = "assets/sounds/102.midi";
+
+  /// Assets for assetsSounds103
+  /// assets/sounds/103.midi
+  static const String assetsSounds103 = "assets/sounds/103.midi";
+
+  /// Assets for assetsSounds104
+  /// assets/sounds/104.midi
+  static const String assetsSounds104 = "assets/sounds/104.midi";
+
+  /// Assets for assetsSounds105
+  /// assets/sounds/105.midi
+  static const String assetsSounds105 = "assets/sounds/105.midi";
+
+  /// Assets for assetsSounds106
+  /// assets/sounds/106.midi
+  static const String assetsSounds106 = "assets/sounds/106.midi";
+
+  /// Assets for assetsSounds107
+  /// assets/sounds/107.midi
+  static const String assetsSounds107 = "assets/sounds/107.midi";
+
+  /// Assets for assetsSounds108
+  /// assets/sounds/108.midi
+  static const String assetsSounds108 = "assets/sounds/108.midi";
+
+  /// Assets for assetsSounds109
+  /// assets/sounds/109.midi
+  static const String assetsSounds109 = "assets/sounds/109.midi";
+
+  /// Assets for assetsSounds111
+  /// assets/sounds/111.midi
+  static const String assetsSounds111 = "assets/sounds/111.midi";
+
+  /// Assets for assetsSounds112
+  /// assets/sounds/112.midi
+  static const String assetsSounds112 = "assets/sounds/112.midi";
+
+  /// Assets for assetsSounds113
+  /// assets/sounds/113.midi
+  static const String assetsSounds113 = "assets/sounds/113.midi";
+
+  /// Assets for assetsSounds114
+  /// assets/sounds/114.midi
+  static const String assetsSounds114 = "assets/sounds/114.midi";
+
+  /// Assets for assetsSounds115
+  /// assets/sounds/115.midi
+  static const String assetsSounds115 = "assets/sounds/115.midi";
+
+  /// Assets for assetsSounds116
+  /// assets/sounds/116.midi
+  static const String assetsSounds116 = "assets/sounds/116.midi";
+
+  /// Assets for assetsSounds117
+  /// assets/sounds/117.midi
+  static const String assetsSounds117 = "assets/sounds/117.midi";
+
+  /// Assets for assetsSounds118
+  /// assets/sounds/118.midi
+  static const String assetsSounds118 = "assets/sounds/118.midi";
+
+  /// Assets for assetsSounds119
+  /// assets/sounds/119.midi
+  static const String assetsSounds119 = "assets/sounds/119.midi";
+
+  /// Assets for assetsSounds124
+  /// assets/sounds/124.midi
+  static const String assetsSounds124 = "assets/sounds/124.midi";
+
+  /// Assets for assetsSounds125
+  /// assets/sounds/125.midi
+  static const String assetsSounds125 = "assets/sounds/125.midi";
+
+  /// Assets for assetsSounds126
+  /// assets/sounds/126.midi
+  static const String assetsSounds126 = "assets/sounds/126.midi";
+
+  /// Assets for assetsSounds130
+  /// assets/sounds/130.midi
+  static const String assetsSounds130 = "assets/sounds/130.midi";
+
+  /// Assets for assetsSounds131
+  /// assets/sounds/131.midi
+  static const String assetsSounds131 = "assets/sounds/131.midi";
+
+  /// Assets for assetsSounds132
+  /// assets/sounds/132.midi
+  static const String assetsSounds132 = "assets/sounds/132.midi";
+
+  /// Assets for assetsSounds137
+  /// assets/sounds/137.midi
+  static const String assetsSounds137 = "assets/sounds/137.midi";
+
+  /// Assets for assetsSounds139
+  /// assets/sounds/139.midi
+  static const String assetsSounds139 = "assets/sounds/139.midi";
+
+  /// Assets for assetsSounds140
+  /// assets/sounds/140.midi
+  static const String assetsSounds140 = "assets/sounds/140.midi";
+
+  /// Assets for assetsSounds145
+  /// assets/sounds/145.midi
+  static const String assetsSounds145 = "assets/sounds/145.midi";
+
+  /// Assets for assetsSounds147
+  /// assets/sounds/147.midi
+  static const String assetsSounds147 = "assets/sounds/147.midi";
+
+  /// Assets for assetsSounds149
+  /// assets/sounds/149.midi
+  static const String assetsSounds149 = "assets/sounds/149.midi";
+
+  /// Assets for assetsSounds153
+  /// assets/sounds/153.midi
+  static const String assetsSounds153 = "assets/sounds/153.midi";
+
+  /// Assets for assetsSounds157
+  /// assets/sounds/157.midi
+  static const String assetsSounds157 = "assets/sounds/157.midi";
+
+  /// Assets for assetsSounds162
+  /// assets/sounds/162.midi
+  static const String assetsSounds162 = "assets/sounds/162.midi";
+
+  /// Assets for assetsSounds163
+  /// assets/sounds/163.midi
+  static const String assetsSounds163 = "assets/sounds/163.midi";
+
+  /// Assets for assetsSounds165
+  /// assets/sounds/165.midi
+  static const String assetsSounds165 = "assets/sounds/165.midi";
+
+  /// Assets for assetsSounds166
+  /// assets/sounds/166.midi
+  static const String assetsSounds166 = "assets/sounds/166.midi";
+
+  /// Assets for assetsSounds167
+  /// assets/sounds/167.midi
+  static const String assetsSounds167 = "assets/sounds/167.midi";
+
+  /// Assets for assetsSounds168
+  /// assets/sounds/168.midi
+  static const String assetsSounds168 = "assets/sounds/168.midi";
+
+  /// Assets for assetsSounds174
+  /// assets/sounds/174.midi
+  static const String assetsSounds174 = "assets/sounds/174.midi";
+
+  /// Assets for assetsSounds178
+  /// assets/sounds/178.midi
+  static const String assetsSounds178 = "assets/sounds/178.midi";
+
+  /// Assets for assetsSounds181
+  /// assets/sounds/181.midi
+  static const String assetsSounds181 = "assets/sounds/181.midi";
+
+  /// Assets for assetsSounds185
+  /// assets/sounds/185.midi
+  static const String assetsSounds185 = "assets/sounds/185.midi";
+
+  /// Assets for assetsSounds187
+  /// assets/sounds/187.midi
+  static const String assetsSounds187 = "assets/sounds/187.midi";
+
+  /// Assets for assetsSounds190
+  /// assets/sounds/190.midi
+  static const String assetsSounds190 = "assets/sounds/190.midi";
+
+  /// Assets for assetsSounds191
+  /// assets/sounds/191.midi
+  static const String assetsSounds191 = "assets/sounds/191.midi";
+
+  /// Assets for assetsSounds192
+  /// assets/sounds/192.midi
+  static const String assetsSounds192 = "assets/sounds/192.midi";
+
+  /// Assets for assetsSounds196
+  /// assets/sounds/196.midi
+  static const String assetsSounds196 = "assets/sounds/196.midi";
+
+  /// Assets for assetsSounds200
+  /// assets/sounds/200.midi
+  static const String assetsSounds200 = "assets/sounds/200.midi";
+
+  /// Assets for assetsSounds202
+  /// assets/sounds/202.midi
+  static const String assetsSounds202 = "assets/sounds/202.midi";
+
+  /// Assets for assetsSounds206
+  /// assets/sounds/206.midi
+  static const String assetsSounds206 = "assets/sounds/206.midi";
+
+  /// Assets for assetsSounds207
+  /// assets/sounds/207.midi
+  static const String assetsSounds207 = "assets/sounds/207.midi";
+
+  /// Assets for assetsSounds210
+  /// assets/sounds/210.midi
+  static const String assetsSounds210 = "assets/sounds/210.midi";
+
+  /// Assets for assetsSounds211
+  /// assets/sounds/211.midi
+  static const String assetsSounds211 = "assets/sounds/211.midi";
+
+  /// Assets for assetsSounds214
+  /// assets/sounds/214.midi
+  static const String assetsSounds214 = "assets/sounds/214.midi";
+
+  /// Assets for assetsSounds215
+  /// assets/sounds/215.midi
+  static const String assetsSounds215 = "assets/sounds/215.midi";
+
+  /// Assets for assetsSounds217
+  /// assets/sounds/217.midi
+  static const String assetsSounds217 = "assets/sounds/217.midi";
+
+  /// Assets for assetsSounds218
+  /// assets/sounds/218.midi
+  static const String assetsSounds218 = "assets/sounds/218.midi";
+
+  /// Assets for assetsSounds219
+  /// assets/sounds/219.midi
+  static const String assetsSounds219 = "assets/sounds/219.midi";
+
+  /// Assets for assetsSounds220
+  /// assets/sounds/220.midi
+  static const String assetsSounds220 = "assets/sounds/220.midi";
+
+  /// Assets for assetsSounds221
+  /// assets/sounds/221.midi
+  static const String assetsSounds221 = "assets/sounds/221.midi";
+
+  /// Assets for assetsSounds228
+  /// assets/sounds/228.midi
+  static const String assetsSounds228 = "assets/sounds/228.midi";
+
+  /// Assets for assetsSounds230
+  /// assets/sounds/230.midi
+  static const String assetsSounds230 = "assets/sounds/230.midi";
+
+  /// Assets for assetsSounds231
+  /// assets/sounds/231.midi
+  static const String assetsSounds231 = "assets/sounds/231.midi";
+
+  /// Assets for assetsSounds234
+  /// assets/sounds/234.midi
+  static const String assetsSounds234 = "assets/sounds/234.midi";
+
+  /// Assets for assetsSounds237
+  /// assets/sounds/237.midi
+  static const String assetsSounds237 = "assets/sounds/237.midi";
+
+  /// Assets for assetsSounds243
+  /// assets/sounds/243.midi
+  static const String assetsSounds243 = "assets/sounds/243.midi";
+
+  /// Assets for assetsSounds247
+  /// assets/sounds/247.midi
+  static const String assetsSounds247 = "assets/sounds/247.midi";
+
+  /// Assets for assetsSounds249
+  /// assets/sounds/249.midi
+  static const String assetsSounds249 = "assets/sounds/249.midi";
+
+  /// Assets for assetsSounds252
+  /// assets/sounds/252.midi
+  static const String assetsSounds252 = "assets/sounds/252.midi";
+
+  /// Assets for assetsSounds255
+  /// assets/sounds/255.midi
+  static const String assetsSounds255 = "assets/sounds/255.midi";
+
+  /// Assets for assetsSounds256
+  /// assets/sounds/256.midi
+  static const String assetsSounds256 = "assets/sounds/256.midi";
+
+  /// Assets for assetsSounds260
+  /// assets/sounds/260.midi
+  static const String assetsSounds260 = "assets/sounds/260.midi";
+
+  /// Assets for assetsSounds264
+  /// assets/sounds/264.midi
+  static const String assetsSounds264 = "assets/sounds/264.midi";
+
+  /// Assets for assetsSounds269
+  /// assets/sounds/269.midi
+  static const String assetsSounds269 = "assets/sounds/269.midi";
+
+  /// Assets for assetsSounds274
+  /// assets/sounds/274.midi
+  static const String assetsSounds274 = "assets/sounds/274.midi";
+
+  /// Assets for assetsSounds276
+  /// assets/sounds/276.midi
+  static const String assetsSounds276 = "assets/sounds/276.midi";
+
+  /// Assets for assetsSounds277
+  /// assets/sounds/277.midi
+  static const String assetsSounds277 = "assets/sounds/277.midi";
+
+  /// Assets for assetsSounds278
+  /// assets/sounds/278.midi
+  static const String assetsSounds278 = "assets/sounds/278.midi";
+
+  /// Assets for assetsSounds282
+  /// assets/sounds/282.midi
+  static const String assetsSounds282 = "assets/sounds/282.midi";
+
+  /// Assets for assetsSounds284
+  /// assets/sounds/284.midi
+  static const String assetsSounds284 = "assets/sounds/284.midi";
+
+  /// Assets for assetsSounds285
+  /// assets/sounds/285.midi
+  static const String assetsSounds285 = "assets/sounds/285.midi";
+
+  /// Assets for assetsSounds288
+  /// assets/sounds/288.midi
+  static const String assetsSounds288 = "assets/sounds/288.midi";
+
+  /// Assets for assetsSounds298
+  /// assets/sounds/298.midi
+  static const String assetsSounds298 = "assets/sounds/298.midi";
+}
+
